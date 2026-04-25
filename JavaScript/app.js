ca31cffc5516a283a001a9d6d100f18407577192
Aditya-raj77x <ar8342077@gmail.com>
2026-04-25 07:10:17 +0530

    some code

---
+++ JavaScript/app.js
@@ -0,0 +1,58 @@
+//let a = ["appke", "banana", "mango", "grappes"]
+// console.log(a);
+// a.push("run");
+// console.log(a);
+// a.push("walk", "why walk");
+// console.log(a);
+// let v = a.pop();
+// console.log(a);
+// console.log(v)
+// a.unshift("111");
+// console.log(a);
+// a.unshift("444", "888", "---");
+// console.log(a);
+// let g = a.shift();
+// console.log(a);
+// console.log(g);
+
+// 2nd wala
+// console.log(a.includes("mango"));
+// console.log(a.includes("Mango"));
+// console.log(a.includes("appke"));
+// console.log(a.includes("appke", 1));
+
+// let a = ["appke", "banana", "mango", "grappes"]
+// console.log(a.indexOf("mango"));
+// console.log(a.indexOf("appke", 1));
+
+// let score = [10, 5, 54, 80, 65, 90]
+// let newData = score.find(s => s > 50);
+// console.log(newData);
+
+// let score = [10, 5, 54, 80, 65, 90]
+// let c = score.slice(1, 3);
+// console.log(c)
+
+let score = [1, 2, 3, 4, 5]
+// score.splice(2, 0, 66);
+// console.log(score)
+// at 3 position
+// score.splice(3, 0, "aditya")
+// console.log(score)
+// replace
+// score.splice(1, 1, "raj");
+// console.log(score)
+
+// score.splice(2, 1);
+score.splice(2, 2);
+
+console.log(score)
+
+
+
+
+
+
+
+
+
